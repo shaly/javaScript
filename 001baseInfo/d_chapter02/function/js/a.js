c9909cfd22cb90d700b51442f29a4aac142203ec
shaly <283199611@qq.com>
2019-03-14 21:48:46 +0800

    IIFE简单演示d_chapter02

---
+++ 001baseInfo/d_chapter02/function/js/a.js
@@ -0,0 +1,6 @@
+//IIFE，Iimmediately invoked function expression即时函数(立即加载的匿名函数)，也叫闭包
+(function () {
+    console.log(this);
+    var a=123,b="Hi javascript";
+    console.log(a);
+})()
---
+++ 001baseInfo/d_chapter02/function/js/c.js
@@ -0,0 +1,5 @@
+//IIFE，Iimmediately invoked function expression即时函数(立即加载的匿名函数)，也叫闭包
+(function () {
+    var a=789,b="Hi css";
+    console.log(a);
+})()
