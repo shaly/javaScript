9dc3af52dd309fda276048f4522f6dfb81536925
shaly <283199611@qq.com>
2019-03-18 22:32:46 +0800

    window无污染，全局变量添加命名空间

--- 001baseInfo/d_chapter05/js/common.js
+++ 001baseInfo/d_chapter05/js/common.js
@@ -1,3 +1,19 @@
 //IIFE，Iimmediately invoked function expression即时函数(立即加载的匿名函数)，也叫闭包
 //全局变量
 var GLOBAL={};
+// GLOBAL.namespaces("A");
+// GLOBAL.namespaces("A.FUN");
+// GLOBAL.namespaces("A.FUN.NAME");
+GLOBAL.namespaces=function (str) {
+    var arr=str.split(".");
+    var obj=GLOBAL;
+    for (let i=(arr[0]=="GLOBAL"?1:0);i<arr.length;i++){
+        obj[arr[i]]=obj[arr[i]]||{};//
+        obj=obj[arr[i]];
+    }
+}
+GLOBAL.namespaces("A");
+GLOBAL.namespaces("GLOBAL.A.XXX1.NAME");
+GLOBAL.namespaces("C");
+GLOBAL.namespaces("B.XXX2.NAME");
+console.log(GLOBAL);
