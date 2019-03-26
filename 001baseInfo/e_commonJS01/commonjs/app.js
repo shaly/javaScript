c5fd47c42364e44db21b630111d19f59446995ff
shaly <283199611@qq.com>
2019-03-26 21:58:30 +0800

    commonJS之node.js返回函数，执行函数

--- 001baseInfo/e_commonJS01/commonjs/app.js
+++ 001baseInfo/e_commonJS01/commonjs/app.js
@@ -1,6 +1,10 @@
 const module1=require("./module");
+
+const module2=require("./module2");
 //console.log(module1);
 console.log(module1);
 console.log(module1.username);
 console.log(module1.age);
 console.log(module1.address);
+
+module2();
---
+++ 001baseInfo/e_commonJS01/commonjs/module2.js
@@ -0,0 +1,3 @@
+module.exports=function () {
+    console.log("module方法");
+}
