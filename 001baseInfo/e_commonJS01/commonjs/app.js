27a7edf80c17edecc0557b26aa89cc6610c835f8
shaly <283199611@qq.com>
2019-03-26 21:53:41 +0800

    commonJS之node.js返回对象

--- 001baseInfo/e_commonJS01/commonjs/app.js
+++ 001baseInfo/e_commonJS01/commonjs/app.js
@@ -1,2 +1,6 @@
 const module1=require("./module");
+//console.log(module1);
 console.log(module1);
+console.log(module1.username);
+console.log(module1.age);
+console.log(module1.address);
--- 001baseInfo/e_commonJS01/commonjs/module.js
+++ 001baseInfo/e_commonJS01/commonjs/module.js
@@ -1,1 +1,13 @@
-module.exports="Jane 测试";
+
+let username="Jane";
+let age=10;
+let address="上海";
+//导出一个值
+//module.exports="Jane 测试";
+//导出一个对象
+module.exports={username:username,age:age,address:address};
+/**
+ *
+ * module.exports:在模块中（js文件），只能存在一个，不能定义多个
+ *
+ **/
