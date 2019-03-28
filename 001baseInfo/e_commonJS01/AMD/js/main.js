2554548969a4106756408c76953894198b54df14
shaly <283199611@qq.com>
2019-03-28 23:13:00 +0800

    AMD模式require导入方式

--- 001baseInfo/e_commonJS01/AMD/js/main.js
+++ 001baseInfo/e_commonJS01/AMD/js/main.js
@@ -8,13 +8,15 @@
         baseURI:"js/",
         //映射路径，模块标识名和路径
         paths:{
-            "util":"modules/util",
-            "userService":"modules/userService"
+            "util":"modules/util",//注意：不要写成modules/util.js
+            "uService":"modules/userService"//uService可以自定义标识名字，require引用时就得引用这个定义的标识名字
         }
     })
 
     //通过require进行导入
-    require(["userService"],function (userModule) {
+    //require(["js/modules/userService.js"],function (userModule) {//这个方式导入也行
+    //推荐使用标识导入，方便简洁明了
+    require(["uService","util"],function (userModule,utilMod) {//引入时的参数是按顺序对应的，变量名自定义
         console.log("主函数引入---》"+userModule.name);
         userModule.searchUser();
     })
