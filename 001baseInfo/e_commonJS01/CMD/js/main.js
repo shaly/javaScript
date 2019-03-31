35586cfbc401cb356de63cd9e3a033b4550f559e
shaly <283199611@qq.com>
2019-03-31 23:00:21 +0800

    CMD加载jquery注意事项

--- 001baseInfo/e_commonJS01/CMD/js/main.js
+++ 001baseInfo/e_commonJS01/CMD/js/main.js
@@ -1,7 +1,8 @@
 define(function (require,exports,module) {
     var u=require("./src/userService");
+    var $=require("./libs/jquery.js");
     console.log(u);
     u.searchUser();
     console.log("main导入userService--->"+u.name);
-
+    $("body").css("background","red");
 })
